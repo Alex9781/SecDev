89dd2cc89ee3b04b8b30a5dbbf2181d107de3a45
Unidentified Squirrel <alex.zmiev.az@gmail.com>
2023-03-18 19:32:57 +0300

    added code protection

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -1,3 +1,10 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace LR1
 {
     internal static class Program
@@ -8,6 +15,16 @@ namespace LR1
         [STAThread]
         static void Main()
         {
+            if (IsCodeHashValid())
+            {
+                MessageBox.Show("Code is valid");
+            }
+            else
+            {
+                MessageBox.Show("Code is invalid");
+                return;
+            }
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 MessageBox.Show("Debugger is attached");
@@ -17,8 +34,28 @@ namespace LR1
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            AuthForm authForm = new AuthForm();
+            AuthForm authForm = new();
             Application.Run(authForm);
         }
+
+        static bool IsCodeHashValid()
+        {
+            byte[] fileBytes = File.ReadAllBytes(Application.ExecutablePath);
+            MemoryStream peStream = new(fileBytes);
+            using var peReader = new PEReader(peStream);
+
+            if (peReader.PEHeaders.PEHeader != null)
+            {
+                PEHeader header = peReader.PEHeaders.PEHeader;
+                int size = header.SizeOfCode;
+                byte[] data = new byte[size];
+                fileBytes.ToList().CopyTo(0x400, data, 0, size);              
+                string currentHash = Convert.ToHexString(SHA256.HashData(data));
+                Debug.WriteLine(currentHash);
+                const string referenceHash = "4F89CC86F8C9DF008B9CEEB91D2D5FC05E2415B75632CEA360571EEE40E24E33";
+                return currentHash == referenceHash;
+            }
+            return false;
+        }     
     }
 }
