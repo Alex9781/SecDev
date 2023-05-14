8b47ab5cc60dcd4ac05b403452d8a04063e8c4bd
Unidentified Squirrel <alex.zmiev.az@gmail.com>
2023-05-14 20:23:43 +0300

    LR2 comp

---
+++ LR2ReadersWriters/ReaderWriterC#/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ReaderWriter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is simple file reader/writer program");
+            Console.WriteLine("Type h for help");
+            ProcessInput();
+        }
+
+        static void ProcessInput()
+        {
+            Console.Write("[read]/[write]: ");
+            string? input = Console.ReadLine();
+
+            if (input != null)
+            {
+                List<string> splitedInput = input.Split(" ").ToList();
+
+                switch (splitedInput[0])
+                {
+                    case "h":
+                        Console.WriteLine("read [file]\nwrite [file]");
+                        break;
+                    case "read":
+                        if (splitedInput.Count == 1)
+                        {
+                            Console.WriteLine("Type h for help");
+                            break;
+                        }
+                        ReadFile(splitedInput[1]);
+                        break;
+                    case "write":
+                        if (splitedInput.Count == 1)
+                        {
+                            Console.WriteLine("Type h for help");
+                            break;
+                        }
+                        WriteFile(splitedInput[1]);
+                        break;
+                    default:
+                        Console.WriteLine("Type h for help");
+                        break;
+                }
+            }
+            ProcessInput();
+        }
+
+        static void ReadFile(string filename)
+        {
+            Console.WriteLine(File.ReadAllText(filename));
+        }
+
+        static void WriteFile(string filename)
+        {
+            string toWrite = DateTime.Now.ToString();
+            File.WriteAllText(filename, toWrite);
+            Console.WriteLine($"Writed: {toWrite}");
+        }
+    }
+}
---
+++ LR2ReadersWriters/ReaderWriterC++/ReaderWriterC++.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+int main()
+{
+	while (true)
+	{
+		std::string input;
+		std::string fileName;
+		std::cout << "[r]/[w]: ";
+		std::cin >> input >> fileName;
+
+		if (input == "r")
+		{
+			FILE* fp;
+			fopen_s(&fp, fileName.c_str(), "r+");
+			char read[1024];
+			if (fp)
+			{
+				fread_s(read, 1024, 1, 1024, fp);
+				std::cout << read;
+				fclose(fp);
+			}
+		}
+		if (input == "w")
+		{
+			FILE* fp;
+			fopen_s(&fp, fileName.c_str(), "r+");
+			if (fp)
+			{
+				auto toWrite = std::chrono::system_clock::now();
+				std::time_t toWriteTime = std::chrono::system_clock::to_time_t(toWrite);
+				auto toWriteChar = std::ctime(&toWriteTime);
+				char newString[50];
+				strcat(newString, toWriteChar);
+				strcat(newString, " ");
+				strcat(newString, toWriteChar);
+
+				std::cout << newString;
+
+				fwrite(newString, strlen(newString), 1, fp);
+				fflush(fp);
+				fclose(fp);
+			}
+		}
+		if (input == "e")
+		{
+			return 0;
+		}
+	}
+}
